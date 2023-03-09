7cdcf5ceebd67902ad0f6415f993fb8eb6969c37
ValeriiHol <rielty@gmail.com>
2023-03-09 22:11:05 +0200

    add task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,53 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const gallery = document.querySelector('.gallery')
+const elements = []
+
+galleryItems.forEach(item => {
+	const galleryLink = document.createElement('a')
+	galleryLink.className = 'gallery__link'
+	galleryLink.href = item.original
+	const galleryImage = document.createElement('img')
+    galleryImage.className = 'gallery__image'
+    galleryImage.src = item.preview;
+    galleryImage.setAttribute('title', item.description)
+    galleryImage.alt = item.description;
+
+	galleryLink.append(galleryImage)
+	elements.push(galleryLink)
+})
+
+
+// const gallery = document.querySelector('.gallery')
+// const items = []
+
+// galleryItems.forEach(element => {
+// 	const galleryLink = document.createElement('a')
+// 	galleryLink.className = 'gallery__link'
+// 	galleryLink.href = element.original
+// 	const galleryImage = document.createElement('img')
+// 	galleryImage.className = 'gallery__image'
+// 	galleryImage.src = element.preview
+// 	galleryImage.setAttribute('title', element.description)
+// 	galleryImage.alt = element.description
+
+// 	galleryLink.append(galleryImage)
+// 	items.push(galleryLink)
+// })
+
+gallery.append(...elements)
+
+
+
+// const lightbox = new SimpleLightbox('.gallery a', {
+//     caption: true,
+//     captionsData: 'alt',
+//     captionDelay: 250,
+//   });
+
+new SimpleLightbox('.gallery a', {
+	captionDelay: 250
+})
