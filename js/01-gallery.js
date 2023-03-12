99074fa61b8b694d2095964148e3111bac522a82
ValeriiHol <rielty@gmail.com>
2023-03-12 17:37:35 +0200

    fix git 01-gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,48 +1,50 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-// import * as basicLightbox from 'basiclightbox'
-console.log(galleryItems);
+// import * as basicLightbox from 'basiclightbox';
+// console.log(galleryItems);
 
-const gallery = document.querySelector('.gallery')
-const elements = []
+const gallery = document.querySelector('.gallery');
+const elements = [];
 
 galleryItems.forEach(item => {
-	const galleryItem = document.createElement('div')
-	galleryItem.className = 'gallery__item'
-	const galleryLink = document.createElement('a')
-	galleryLink.className = 'gallery__link'
-	galleryLink.href = item.original
-	const galleryImage = document.createElement('img')
-    galleryImage.className = 'gallery__image'
-    galleryImage.src = item.preview;
-    galleryImage.setAttribute('data-source', item.original)
-    galleryImage.alt = item.description;
-
-
-	galleryItem.append(galleryLink)
-	galleryLink.append(galleryImage)
-	elements.push(galleryItem)
+	const galleryItem = `<div class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+    <img class="gallery__image" src="${item.preview}"
+    data-source="${item.original}"alt="${item.description}"/>
+    </a>
+    </div>`;
+	elements.push(galleryItem);
 })
 
-gallery.append(...elements)
+gallery.insertAdjacentHTML('beforeend', elements);
 
 gallery.addEventListener('click', event => {
     event.preventDefault();
     if (event.target.nodeName !== 'IMG') {
-		return
+		return;
 	}
 
     // const selectedImg = event.target.getAttribute('data-source')
+	const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">`,
+    {
+    onShow: () => {
+        console.log('addEventListener');
+        window.addEventListener('keydown', onPressEsc);
+    },
+    onClose: () => {
+        console.log('removeEventListener');
+        window.removeEventListener('keydown', onPressEsc);
+    }
+    });
 
-    const instance = basicLightbox.create(`
-    <img src="${event.target.dataset.source}" width="800" height="600">
-`)
+    const onPressEsc = event => {
+        console.log(event.code);
+        if (event.code === 'Escape') {
+            console.log('Escape');
+            instance.close();
+        }
+      };
 
-    instance.show()
-    
-    gallery.addEventListener('keydown', event => {
-		if (event.key === 'Escape') {
-			instance.close()
-		}
-	})
+    instance.show();
 })
